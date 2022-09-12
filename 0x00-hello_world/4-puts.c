46c6f4f9d18c797e65c40b40fdebc8abc212b25b
Alemayehu <lualemayehu@gmail.com>
2022-09-12 11:53:18 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 /** 
  * main - Entry point
-
+ *
  * this program prints a line using puts
  */
 
 int main(void)
 {
-
-	puts ("\"Programming is like building a multilingual puzzle");
-	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
