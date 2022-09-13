63deb59246225cc3d2576ca7a63ca1f882fabe19
Alemayehu <lualemayehu@gmail.com>
2022-09-13 06:34:42 -0700

    task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,8 +4,7 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
- *
- * Program prints numbers with a single degit starting from 0 
+ * Program prints numbers with a single degit starting from 0
  */
 
 int main(void)
