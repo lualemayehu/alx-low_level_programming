36daab0d60a08e48bb80cfb61e28f0c14c553931
Alemayehu <lualemayehu@gmail.com>
2022-09-14 02:47:45 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Program prints alpahabates in lower case
+ */
+
+void print_alphabet(void)
+{
+	char n = 'a';
+
+	while (n < '{')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
