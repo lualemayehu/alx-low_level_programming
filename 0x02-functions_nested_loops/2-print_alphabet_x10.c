eea9c463dd0503279702cbb4297803e6518dd725
Alemayehu <lualemayehu@gmail.com>
2022-09-14 07:10:57 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet 10X- Entry point
+ *
+ * Program prints alpahabates in lower case 10X
+ */
+
+void print_alphabet_x10(void)
+{
+	char n = 'a';
+	int i;
+
+	for (i = 0; i <=10 ; i++)
+	{
+	while (n < '{')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+	}
+}
