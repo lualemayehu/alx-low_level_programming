513dc06635cc1e71fcade8bbd930a5c820ceeade
Alemayehu <lualemayehu@gmail.com>
2022-09-14 10:01:49 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_sign - checks for the sign
+ *
+ *@n: it takes integer parameter c as an input
+ *
+ * Returns 1 and prints + if n is greater than zero
+ * Returns 0 and prints 0 if n is zero
+ * Returns -1 and prints - if n is less than zero
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return(0);
+	}
+	else
+	{
+		_putchar(45);
+		return(-1);
+	}
+}
