3ab3bb438075a7914bd130174b581a42f0c09a02
Alemayehu <lualemayehu@gmail.com>
2022-09-15 01:26:11 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _abs - display absolute value
+ *
+ *@n: it takes integer parameter n as an input
+ *
+ * Return: absolute
+ */
+
+int _abs(int n)
+{
+	
+	n =abs(n);
+	
+	return (n);
+}
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,24 +1,30 @@
 #include "main.h"
 /**
  * print_last_digit - display last digit
- * 
- *@n: it takes integer parameter n as an input
+ *
+ * @n: it takes integer parameter n as an input
  *
  * Return: last digit
  */
 
 int print_last_digit(int n)
 {
+	char j;
+
 	if (n < 0)
 	{
 		n = n * -1;
-		_putchar((n % 10) + '0');
-		return (n % 10);
+		n = (n % 10);
+		j = (char)n;
+		_putchar(j);
+		return (n);
 	}
 	else if (n > 0)
 	{
-		_putchar((n % 10) + '0');
-		return (n % 10);	
+		n = (n % 10);
+		j = (char)n;
+		_putchar(j);
+		return (n);
 	}
 	else if (n == 0)
 	{
