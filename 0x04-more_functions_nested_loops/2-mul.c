5b2a7a207ca4a88dcf5e62187884e55c15736f8d
Alemayehu <lualemayehu@gmail.com>
2022-09-22 03:50:26 -0700

    task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * _isupper - checks if the input is upper case
+ *
+ * @a: input 1
+ * 
+ * @b: input 2
+ *
+ * Return: multiplication of a and b
+ */
+
+int mul(int a ,int b)
+{
+	return ( a * b);
+}
