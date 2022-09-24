e2dad8809a936f5d2a54b6df251d8a63ff019af7
Alemayehu <lualemayehu@gmail.com>
2022-09-24 04:14:44 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - count number of a string
+ * @s: string pointer
+ *
+ * Return: number of strings
+ */
+
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+	
+	for (i = 0; s[i] != '\0'; ++i)
+		count++;
+	return (count);
+}
